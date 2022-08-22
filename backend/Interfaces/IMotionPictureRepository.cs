d598078840dae692ac640665bc55478319c46785
samkcrespo <samkcrespo@gmail.com>
2022-08-22 14:07:56 -0700

    rm frontend files, will add once backend completed
    re-arranged settings.json, tested endpoints in swagger. success

--- backend/Interfaces/IMotionPictureRepository.cs
+++ backend/Interfaces/IMotionPictureRepository.cs
@@ -5,5 +5,10 @@ namespace backend.Interfaces
     public interface IMotionPictureRepository
     {
         ICollection<MotionPicture> GetMotionPictures();
+        //MotionPicture GetMotionPicture(int id);
+        //MotionPicture GetMotionPicture(string name);
+        
+        
+
     }
 }
